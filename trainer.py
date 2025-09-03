ef2523c0be45d84bf7b11d9914cb653510a8c83f
Dr. Christian Bahls <donotspamme@qb352.de>
2025-09-03 23:32:33 +0200

    change how recall is printed

--- trainer.py
+++ trainer.py
@@ -252,9 +252,6 @@ class AttachedPooledEmbedderTrainer(Trainer):
         return (loss, sim_student) if return_outputs else loss
 
 
-from transformers import TrainerCallback
-
-
 class RecallEvaluationCallback(TrainerCallback):
     def __init__(
         self,
@@ -315,12 +312,14 @@ class RecallEvaluationCallback(TrainerCallback):
             rerank_k=self.rerank_k,
         )
 
+        print("Recall:", end="")
         # Log results safely
         for k, recall_val in recalls.items():
             state.log_history.append(
                 {"eval_recall@{}".format(k): recall_val, "step": state.global_step}
             )
-            print(f"recall@{k}: {recall_val:.4f}")
+            print(f" @{k}:{recall_val:.2f}", end="")
+        print(".")
 
 
 def main():
